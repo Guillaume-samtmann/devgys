00d47a8400cc4018782cef2c4db6b7a7e5c39888
SAMTMANN GUILLAUME <guillaumesamtmann24@gmail.com>
2023-06-29 10:34:03 +0000

    deuxime commit

--- index.php
+++ index.php
@@ -3,9 +3,84 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>Tabat</title>
 </head>
 <body>
+<header>
+<nav class="navbar " role="navigation">
+        <div class="navbar__logo">
+            <ul>
+                <li>
+                    <a class="logo" href="index.php"><img src="img/logo.png" alt=""></a>
+                </li>
+            </ul>
+        </div>
+        <ul class="navbar__links">
+            <li class="navbar__link first"><a href="index.php">Accueil</a></li>
+            <li class="navbar__link second"><a href="listing.php">Gros oeuvre</a></li>
+            <li class="navbar__link third"><a href="form_recherche.php">Sciage</a></li>
+            <li class="navbar__link four"><a href="./admin/admin.php">Carottage</a></li>
+            <li class="navbar__link five"><a href="./admin/admin.php">Démolition</a></li>
+            <li class="navbar__link six"><a href="./admin/admin.php">Curage</a></li>
+            <li class="navbar__link seven"><a href="./admin/admin.php">Contact</a></li>
+        </ul>
+        <button class="burger">
+            <span class="bar"></span>
+    
+        </button>
+    </nav>
+    <script src="js.js"></script>
+</header>
+<main>
+    <div class="intro">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta veniam provident quo! Explicabo officia similique dolorum 
+            aliquam asperiores, dignissimos molestias facere sequi dolore, architecto culpa ullam veritatis. Tenetur, molestiae alias?
+        </p>
+    </div>
+    <div class="descriptiontabat">
+        <h1>Qui sommes nous ?</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo veritatis suscipit id eligendi animi ab 
+            voluptatibus minus neque illum eos harum, ex, tenetur obcaecati officiis nostrum sunt reprehenderit magnam. Molestias!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo veritatis suscipit id eligendi animi ab 
+            voluptatibus minus neque illum eos harum, ex, tenetur obcaecati officiis nostrum sunt reprehenderit magnam. Molestias!
+        </p>
+        <button>En savoir plus !</button>
+    </div>
+    <div class="lesactivites">
+        <div class="gros-oeuvre">
+            <h2>Gros oeuvre</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo asperiores porro perspiciatis facere ab fugiat, 
+                laborum, accusantium perferendis eveniet aspernatur dicta voluptatum, alias cumque esse deserunt nobis itaque officia expedita.
+            </p>          
+        </div>
+        <div class="sciage">
+            <h2>Sciage</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo asperiores porro perspiciatis facere ab fugiat, 
+                laborum, accusantium perferendis eveniet aspernatur dicta voluptatum, alias cumque esse deserunt nobis itaque officia expedita.
+            </p>
+        </div>
+        <div class="carottage">
+            <h2>Carottage</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo asperiores porro perspiciatis facere ab fugiat, 
+            laborum, accusantium perferendis eveniet aspernatur dicta voluptatum, alias cumque esse deserunt nobis itaque officia expedita.
+        </p>
+        </div>
+        <div class="demolition">
+            <h2>Démolition</h2>  
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo asperiores porro perspiciatis facere ab fugiat, 
+                laborum, accusantium perferendis eveniet aspernatur dicta voluptatum, alias cumque esse deserunt nobis itaque officia expedita.
+            </p>
+        </div>
+        <div class="curage">
+            <h2>Curage</h2>  
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo asperiores porro perspiciatis facere ab fugiat, 
+                laborum, accusantium perferendis eveniet aspernatur dicta voluptatum, alias cumque esse deserunt nobis itaque officia expedita.
+            </p>           
+        </div>
+    </div>
+</main>
+    
     
 </body>
 </html>
